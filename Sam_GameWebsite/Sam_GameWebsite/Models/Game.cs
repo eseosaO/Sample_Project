16bc1102c2cc94d6106728ac90823136e0599457
eseosaO <eseosaoshodin@yahoo.com>
2020-03-25 18:52:57 +0000

    MVC Setup and Scaffolding Game Model class

---
+++ Sam_GameWebsite/Sam_GameWebsite/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sam_GameWebsite.Models
+{
+    public class Game
+    {
+        public virtual int GameId { get; set; }
+        public virtual int GenreId { get; set; }
+        public virtual string GameName { get; set; }
+        public virtual string GameDescription { get; set; }
+        public virtual Genre genre { get; set; }
+        public virtual decimal Price { get; set; }
+    }
+}
---
+++ Sam_GameWebsite/Sam_GameWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sam_GameWebsite.Startup))]
+namespace Sam_GameWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
